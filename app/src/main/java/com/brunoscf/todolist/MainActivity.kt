f7675e4c69e24afae12c67cbb5ad0c1dd1178674
brunoscf <brunoscf5@outlook.com>
2021-09-04 12:16:05 -0400

    feat: add AddTaskActivity

--- app/src/main/java/com/brunoscf/todolist/MainActivity.kt
+++ app/src/main/java/com/brunoscf/todolist/MainActivity.kt
@@ -1,11 +1,30 @@
 package com.brunoscf.todolist
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.google.android.material.floatingactionbutton.FloatingActionButton
+import com.google.android.material.textfield.TextInputEditText
 
 class MainActivity : AppCompatActivity() {
+    lateinit var btnAddTask: FloatingActionButton
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        initData()
+        initListeners()
+    }
+
+    fun initData(){
+        btnAddTask = findViewById(R.id.add_task)
+    }
+
+    fun initListeners(){
+        btnAddTask.setOnClickListener {
+            val intent = Intent(this, AddTaskActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
